f3b45cd7d25f147040935d76d7612cf7db62ebb9
Berecht <Berecht@users.noreply.github.com>
2018-10-05 13:56:07 +0200

    Update preprocessing.R

--- preprocessing.R
+++ preprocessing.R
@@ -2,7 +2,6 @@ setwd("~/B/VU/2018-2019/AML/Kaggle")
 
 library(data.table)
 
-
 airports <- data.table::fread("airports.csv")
 sample_submission <- data.table::fread("sample_submission.csv")
 test <- data.table::fread("test.csv")
@@ -11,13 +10,10 @@ weather <- data.table::fread("weather.csv")
 
 weather$time_hour <- as.POSIXct(weather$time_hour, format = "%Y-%m-%d %H:%M:%S")
 
-
-
 train$sched_dep_time <- floor(ifelse(train$sched_dep_time %% 100 >= 30,train$sched_dep_time+100,train$sched_dep_time)/100)*100
 train$sched_arr_time <- floor(ifelse(train$sched_arr_time %% 100 >= 30,train$sched_arr_time+100,train$sched_arr_time)/100)*100
 train$sched_arr_time <- ifelse(train$sched_arr_time == 2400,0000,train$sched_arr_time)
 train$sched_dep_time <- ifelse(train$sched_dep_time == 2400,0000,train$sched_dep_time)
-
 
 train$dep_hour <- sub("(.*)(.{2})", "\\1:\\2:00", sprintf("%04d", train$sched_dep_time))
 train$arr_hour <- sub("(.*)(.{2})", "\\1:\\2:00", sprintf("%04d", train$sched_arr_time))
@@ -38,14 +34,11 @@ data.table::setnames(temp_weather,c("origin","arr_temp", "arr_dewp", "arr_humid"
 train2 <- merge(train2,temp_weather, by.x = c("arr_date","dest"), by.y = c("time_hour","origin") ,all.x = TRUE)
 rm(temp_weather)
 
-
-temp_airports <- airports[,c("faa", "name", "lat", "lon", "alt", "tz", "dst", "tzone")]
-
-
+temp_airports <- airports[,c("faa", "name", "lat", "lon", "alt", "tz", "dst", "tzone")
 
 data.table::setnames(temp_airports,c("faa", "dep_name", "dep_lat", "dep_lon", "dep_alt", "dep_tz", "dep_dst", "dep_tzone"))
 train3 <- merge(train2,temp_airports, by.x = "origin", by.y = "faa" ,all.x = TRUE)
 data.table::setnames(temp_airports,c("faa", "arr_name", "arr_lat", "arr_lon", "arr_alt", "arr_tz", "arr_dst", "arr_tzone"))
 train3 <- merge(train3,temp_airports, by.x = "dest", by.y = "faa" ,all.x = TRUE)
 
-
+write.csv(train3,"train_full.csv")
