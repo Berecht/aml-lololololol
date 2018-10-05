e6d5958ae517bff6fc65a53d99c8ad1ebf7cdc62
Berecht <Berecht@users.noreply.github.com>
2018-10-05 14:46:04 +0200

    Update preprocessing.R

--- preprocessing.R
+++ preprocessing.R
@@ -10,20 +10,20 @@ weather <- data.table::fread("weather.csv")
 
 weather$time_hour <- as.POSIXct(weather$time_hour, format = "%Y-%m-%d %H:%M:%S")
 
-train$sched_dep_time <- floor(ifelse(train$sched_dep_time %% 100 >= 30,train$sched_dep_time+100,train$sched_dep_time)/100)*100
-train$sched_arr_time <- floor(ifelse(train$sched_arr_time %% 100 >= 30,train$sched_arr_time+100,train$sched_arr_time)/100)*100
-train$sched_arr_time <- ifelse(train$sched_arr_time == 2400,0000,train$sched_arr_time)
-train$sched_dep_time <- ifelse(train$sched_dep_time == 2400,0000,train$sched_dep_time)
+train$temp_sched_dep_time <- floor(ifelse(train$sched_dep_time %% 100 >= 30,train$sched_dep_time+100,train$sched_dep_time)/100)*100
+train$temp_sched_arr_time <- floor(ifelse(train$sched_arr_time %% 100 >= 30,train$sched_arr_time+100,train$sched_arr_time)/100)*100
+train$temp_sched_arr_time <- ifelse(train$temp_sched_arr_time == 2400,0000,train$temp_sched_arr_time)
+train$temp_sched_dep_time <- ifelse(train$temp_sched_dep_time == 2400,0000,train$temp_sched_dep_time)
 
-train$dep_hour <- sub("(.*)(.{2})", "\\1:\\2:00", sprintf("%04d", train$sched_dep_time))
-train$arr_hour <- sub("(.*)(.{2})", "\\1:\\2:00", sprintf("%04d", train$sched_arr_time))
+train$dep_hour <- sub("(.*)(.{2})", "\\1:\\2:00", sprintf("%04d", train$temp_sched_dep_time))
+train$arr_hour <- sub("(.*)(.{2})", "\\1:\\2:00", sprintf("%04d", train$temp_sched_arr_time))
+train[, c("temp_sched_dep_time","temp_sched_arr_time") := NULL]
 train$dep_date <- as.POSIXct(paste0(train$year, "-", sprintf("%02d",train$month), "-", sprintf("%02d",train$day), " ", train$dep_hour), format = "%Y-%m-%d %H:%M:%S")
 train$arr_date <- as.POSIXct(paste0(train$year, "-", sprintf("%02d",train$month), "-", sprintf("%02d",train$day), " ", train$arr_hour), format = "%Y-%m-%d %H:%M:%S")
 
 temp_weather <- weather[,c("origin","temp", "dewp", "humid", "wind_dir", 
                            "wind_speed", "wind_gust", "precip", "pressure", 
                            "visib", "time_hour")]
-
 data.table::setnames(temp_weather,c("origin","dep_temp", "dep_dewp", "dep_humid", "dep_wind_dir", 
                                     "dep_wind_speed", "dep_wind_gust", "dep_precip", "dep_pressure", 
                                     "dep_visib", "time_hour"))
@@ -35,11 +35,9 @@ train2 <- merge(train,temp_weather,by.x = c("dep_date","origin"), by.y = c("time
 rm(temp_weather)
 
 temp_airports <- airports[,c("faa", "name", "lat", "lon", "alt", "tz", "dst", "tzone")]
-
 data.table::setnames(temp_airports,c("faa", "dep_name", "dep_lat", "dep_lon", "dep_alt", "dep_tz", "dep_dst", "dep_tzone"))
 train3 <- merge(train2,temp_airports, by.x = "origin", by.y = "faa" ,all.x = TRUE)
 data.table::setnames(temp_airports,c("faa", "arr_name", "arr_lat", "arr_lon", "arr_alt", "arr_tz", "arr_dst", "arr_tzone"))
 train3 <- merge(train3,temp_airports, by.x = "dest", by.y = "faa" ,all.x = TRUE)
-
+rm(temp_airports)
 write.csv(train3,"train_full.csv")
-
